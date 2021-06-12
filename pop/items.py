73ec666bf20140fd3a690f70163b84a8352ac953
Bintang Ginanjar <bintang.ginanjar@gmail.com>
2021-06-12 22:26:00 +0700

    First commit

---
+++ pop/items.py
@@ -0,0 +1,23 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+from scrapy.item import Item, Field
+from itemloaders.processors import MapCompose, TakeFirst, Join
+
+def removeNewLine(text):
+        text = text.split('\n')
+        text = text[0].lstrip()
+
+        return text
+
+class PopItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    nama = Field(input_processor = MapCompose(removeNewLine), output_processor = TakeFirst())
+    artiNama = Field(output_processor = Join(separator = '; '))
+    
+    pass
---
+++ pop/spiders/pop-spider.py
@@ -0,0 +1,51 @@
+import scrapy
+import string
+
+from scrapy.loader import ItemLoader
+from pop.items import PopItem
+
+class PopSpider(scrapy.Spider):
+	name = 'pop'
+
+	custom_settings = {
+        'FEED_EXPORT_FIELDS': [
+            'nama',
+            'artiNama'
+        ]
+    }
+
+	def start_requests(self):
+		sexList = ['male', 'female']
+		alphabetList = list(string.ascii_lowercase)
+
+		baseUrl = 'https://www.popmama.com/baby-name'
+		
+		for sex in sexList:
+			for alphabet in alphabetList:
+				targetUrl = baseUrl + '/' + sex + '/' + alphabet
+
+				prodResponse = scrapy.Request(targetUrl, callback = self.parse)
+				prodResponse.meta['dont_cache'] = True
+
+				yield prodResponse
+
+	def parse(self, response):		
+		rows = response.css('div.result-content li')		
+
+		for row in rows:
+			loader = ItemLoader(item = PopItem(), selector = row)
+			loader.add_css('nama', 'a::text')
+
+			nameItem = loader.load_item()
+			detailUrl = row.css('a::attr(href)').get()
+
+			yield response.follow(detailUrl, self.parseDetail, meta = {'nameItem': nameItem})
+		
+
+	def parseDetail(self, response):
+		nameItem = response.meta['nameItem']
+
+		loader = ItemLoader(item = nameItem, response = response)
+		loader.add_css('artiNama', 'div.description > ul > li::text')
+
+		yield loader.load_item()
